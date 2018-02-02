458a62cb92666b9af4cb2ff9778bb14fa4882403
danivsainz <danivsainz@gmail.com>
2018-02-02 03:53:58 -0500

    More Code

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
@@ -12,9 +12,16 @@
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
+import tensorflow as tf
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
 from keras.models import Sequential
-from keras.layers import Conv2D
+from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
+classifier = Sequential()
+
+#Adding convulution
+classifier.add(Convolution2D(32, 3 , 3,input_shape(64,64,3), activation='relu'))
