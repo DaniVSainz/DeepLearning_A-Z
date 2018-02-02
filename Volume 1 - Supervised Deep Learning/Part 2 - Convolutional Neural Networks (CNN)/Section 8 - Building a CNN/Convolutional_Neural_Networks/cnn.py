0c52a3ab5b14031d0bd65e4f296b991f884ab8dc
danivsainz <danivsainz@gmail.com>
2018-02-02 07:32:56 -0500

    Run test on 6 images by hardcoding model produced accurate results with normal looking cats

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
@@ -56,13 +56,13 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
         'dataset/training_set',
         target_size=(64, 64),
-        batch_size=64,
+        batch_size=128,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
         target_size=(64, 64),
-        batch_size=64,
+        batch_size=128,
         class_mode='binary')
 
 import tensorflow as tf
@@ -73,7 +73,31 @@ with tf.device('/gpu:1'):
                          epochs = 25,
                          validation_data = test_set,
                          validation_steps = (2000/32),
-                          use_multiprocessing=True, workers=8)
+                         workers=8)
+
+import numpy as np
+from keras.preprocessing import image
+
+test_image = image.load_img('dataset/single_prediction/4.jpg',  target_size=(64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image,axis=0)
+
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+print(prediction)
+
+
+
+
+
+
+
+
 
 
 
