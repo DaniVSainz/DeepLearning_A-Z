66aaf01773c9b842c8dc144795e034ed54fc3970
danivsainz <danivsainz@gmail.com>
2018-02-02 05:27:29 -0500

    Add gpu support

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
@@ -51,24 +51,25 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
         'dataset/training_set',
         target_size=(64, 64),
-        batch_size=32,
+        batch_size=128,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
         target_size=(64, 64),
-        batch_size=32,
+        batch_size=128,
         class_mode='binary')
 
 import tensorflow as tf
 
-with tf.device('/gpu:2'):
+with tf.device('/gpu:1'):
     classifier.fit_generator(
             training_set,
-            steps_per_epoch=8000,
+            steps_per_epoch=62,
             epochs=25,
             validation_data=test_set,
-            validation_steps=2000)
+            validation_steps=10,
+            )
 
 
 
