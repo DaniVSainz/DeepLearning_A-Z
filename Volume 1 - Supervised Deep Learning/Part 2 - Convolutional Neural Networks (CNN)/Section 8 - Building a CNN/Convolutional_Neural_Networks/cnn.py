5eaaa50bb73a191f146fb37f735c96f2f895e762
danivsainz <danivsainz@gmail.com>
2018-02-02 07:18:33 -0500

    Finish CNN with an accuracy of 95%

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
@@ -27,6 +27,11 @@ classifier = Sequential()
 classifier.add(Convolution2D(32,(3,3),input_shape=(64,64,3), activation='relu'))
 #Max Pooling Step,
 classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+#Add another convulutional layer to further increase accuracy
+classifier.add(Convolution2D(32,(3,3), activation='relu'))
+#Max Pooling Step,
+classifier.add(MaxPooling2D(pool_size = (2,2)))
 #Flattening step
 classifier.add(Flatten())
 #Step 4 Fully Connect
@@ -51,25 +56,24 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
         'dataset/training_set',
         target_size=(64, 64),
-        batch_size=128,
+        batch_size=64,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
         target_size=(64, 64),
-        batch_size=128,
+        batch_size=64,
         class_mode='binary')
 
 import tensorflow as tf
 
 with tf.device('/gpu:1'):
-    classifier.fit_generator(
-            training_set,
-            steps_per_epoch=62,
-            epochs=25,
-            validation_data=test_set,
-            validation_steps=10,
-            )
+    classifier.fit_generator(training_set,
+                         steps_per_epoch = (8000/32),
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = (2000/32),
+                          use_multiprocessing=True, workers=8)
 
 
 
