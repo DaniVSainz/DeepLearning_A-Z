f8822066f57b19f8c39d32bf99276ae46e059292
danivsainz <danivsainz@gmail.com>
2018-02-02 19:10:00 -0500

    Import data sets for Recurrent neural network with the goal of predicting google stock price trends, upwards or downwards

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
@@ -20,6 +20,21 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from tensorflow.contrib.keras.api.keras.callbacks import Callback
+
+class LossHistory(Callback):
+    def __init__(self):
+        super().__init__()
+        self.epoch_id = 0
+        self.losses = ''
+ 
+    def on_epoch_end(self, epoch, logs={}):
+        self.losses += "Epoch {}: accuracy -> {:.4f}, val_accuracy -> {:.4f}\n"\
+            .format(str(self.epoch_id), logs.get('acc'), logs.get('val_acc'))
+        self.epoch_id += 1
+ 
+    def on_train_begin(self, logs={}):
+        self.losses += 'Training begins...\n'
 
 classifier = Sequential()
 
@@ -44,6 +59,7 @@ classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [
 
 #Image preprocessing
 from keras.preprocessing.image import ImageDataGenerator
+batch_size = 256
 
 train_datagen = ImageDataGenerator(
         rescale=1./255,
@@ -56,29 +72,32 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
         'dataset/training_set',
         target_size=(64, 64),
-        batch_size=128,
+        batch_size=batch_size,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
         target_size=(64, 64),
-        batch_size=128,
+        batch_size=batch_size,
         class_mode='binary')
 
 import tensorflow as tf
+history = LossHistory()
 
 with tf.device('/gpu:1'):
     classifier.fit_generator(training_set,
-                         steps_per_epoch = (8000/32),
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = (2000/32),
-                         workers=8)
+                             steps_per_epoch=8000/batch_size,
+                             epochs=90,
+                             validation_data=test_set,
+                             validation_steps=2000/batch_size,
+                             workers=8,
+#                             max_q_size=300,
+                             callbacks=[history])
 
 import numpy as np
 from keras.preprocessing import image
 
-test_image = image.load_img('dataset/single_prediction/4.jpg',  target_size=(64, 64))
+test_image = image.load_img('dataset/single_prediction/6.jpg',  target_size=(64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image,axis=0)
 
