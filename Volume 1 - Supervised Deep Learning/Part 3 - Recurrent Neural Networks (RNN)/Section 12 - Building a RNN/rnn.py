3542224ea5fe17a9bf6c5456b9afcb6759272b62
danivsainz <danivsainz@gmail.com>
2018-02-02 22:14:16 -0500

    Fit our data and add imports

--- Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
+++ Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -10,3 +10,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+#Import our training set
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:,1:2].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range=(0,1))
+training_set_scaled = sc.fit_transform(training_set)
+
+#Create a data structure with 60 timesteps and 1 output 
+X_train = []
+y_train = []
+
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+X_train = np.reshape(X_train,(X_train.shape[0], X_train.shape[1], 1))
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+regressor = Sequential()
