467caf17b22c8a78237ce6c5cc51e3c660cf108c
danivsainz <danivsainz@gmail.com>
2018-02-03 01:34:52 -0500

    Finish RNN and generate charts off of our model

--- Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
+++ Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -36,35 +36,56 @@ from keras.layers import Dropout
 
 regressor = Sequential()
 #Create first LSTM layer
-regressor.add(LSTM(units = 50, return_sequences = True , input_shape = (X_train.shape[1], 1)))
+regressor.add(LSTM(units = 500, return_sequences = True , input_shape = (X_train.shape[1], 1)))
 regressor.add(Dropout(0.2))
 
 #Second layer with more dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(LSTM(units = 500, return_sequences = True))
 regressor.add(Dropout(0.2))
 
 #Third layer
-regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(LSTM(units = 500, return_sequences = True))
 regressor.add(Dropout(0.2))
 
+
 #Fourth layer
-regressor.add(LSTM(units = 50, return_sequences = False))
+regressor.add(LSTM(units = 500, return_sequences = False))
 regressor.add(Dropout(0.2))
 
 regressor.add(Dense(units = 1))
 
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error' )
 
-regressor.fit(X_train,y_train, epochs = 100 , batch_size = 124)
+regressor.fit(X_train,y_train, epochs = 100 , batch_size = 256)
 
 
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
 real_stock_price = dataset_test.iloc[:,1:2].values
 
 
+dataset_total = pd.concat((dataset_train["Open"], dataset_test["Open"]), axis=0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
 
+X_test = []
 
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test,(X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
+#Visualizing the results
+
+plt.plot(real_stock_price, color='blue', label='Real Google Stock Price')
+plt.plot(predicted_stock_price, color='red', label='Predicted Google Stock Price')
+plt.title("Google Stock Price Prediction")
+plt.xlabel('Time')
+plt.ylabel('Google stock price')
+plt.legend()
+plt.show()
 
 
 
