ae21bc7b627e665f8f69c436afcf39e94ada7fb0
danivsainz <danivsainz@gmail.com>
2018-02-02 23:47:56 -0500

    Import real stock prices

--- Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
+++ Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -35,3 +35,42 @@ from keras.layers import LSTM
 from keras.layers import Dropout
 
 regressor = Sequential()
+#Create first LSTM layer
+regressor.add(LSTM(units = 50, return_sequences = True , input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+#Second layer with more dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+#Third layer
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+#Fourth layer
+regressor.add(LSTM(units = 50, return_sequences = False))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units = 1))
+
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error' )
+
+regressor.fit(X_train,y_train, epochs = 100 , batch_size = 124)
+
+
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:,1:2].values
+
+
+
+
+
+
+
+
+
+
+
+
+
+
